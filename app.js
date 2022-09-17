29f5edb1830eafda4f1f100e65d993494ab899c6
Thakur Saad <thakursaad613@gmail.com>
2022-09-17 23:50:37 +0600

    projection(-ve)

--- app.js
+++ app.js
@@ -151,7 +151,7 @@ app.post("/api/v1/product", async (req, res, next) => {
 
 app.get("/api/v1/product", async (req, res, next) => {
   try {
-    const products = await Product.find({}, "name quantity");
+    const products = await Product.find({}, "-name -quantity");
 
     res.status(200).json({
       status: "Success",
