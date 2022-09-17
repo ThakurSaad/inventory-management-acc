beb287263c100555d7d31eccdccf68c168853e5f
Thakur Saad <thakursaad613@gmail.com>
2022-09-17 13:12:36 +0600

    Schema-Design

--- app.js
+++ app.js
@@ -7,6 +7,86 @@ const mongoose = require("mongoose");
 app.use(express.json());
 app.use(cors());
 
+// schema design
+const productSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please provide a name for this product"],
+      trim: true,
+      unique: [true, "Name must be unique"],
+      minLength: [3, "Name must be at least 3 characters"],
+      maxLength: [100, "Name is too large"],
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+      min: [0, "Price can not be negative"],
+    },
+    unit: {
+      type: String,
+      required: true,
+      enum: {
+        values: ["kg", "litre", "pcs"],
+        message: "unit value can not be value, must be kg/liter/pcs",
+      },
+    },
+    quantity: {
+      type: Number,
+      required: true,
+      min: [0, "Quantity can not be negative"],
+      validate: {
+        validator: (value) => {
+          const isInteger = Number.isInteger(value);
+
+          if (isInteger) {
+            return true;
+          } else {
+            return false;
+          }
+        },
+      },
+      message: "Quantity must be an integer",
+    },
+    status: {
+      type: String,
+      required: true,
+      enum: {
+        values: ["in-stock", "out-of-stock", "discontinued"],
+        message: "Status can not be {VALUE}",
+      },
+    },
+    // createdAt: {
+    //   type: Date,
+    //   default: Date.now,
+    // },
+    // updatedAt: {
+    //   type: Date,
+    //   default: Date.now,
+    // },
+    supplier: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Supplier",
+    },
+    categories: [
+      {
+        name: {
+          type: String,
+          required: true,
+        },
+        _id: mongoose.Schema.Types.ObjectId,
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
 app.get("/", (req, res) => {
   res.send("Route is working! YaY!");
 });
--- server.js
+++ server.js
@@ -5,13 +5,13 @@ const colors = require("colors");
 const app = require("./app");
 
 // database connection
-mongoose.connect(process.env.DATABASE).then(() => {
-  console.log("Database connection is successful");
+mongoose.connect(process.env.DATABASE_LOCAL).then(() => {
+  console.log(`Database connection is successful`.bgWhite.bold);
 });
 
 // server
 const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
-  console.log(`App is running on port ${port}`.yellow.bold);
+  console.log(`App is running on port ${port}`.rainbow);
 });
