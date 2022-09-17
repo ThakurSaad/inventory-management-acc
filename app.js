829951a4b2db8551a2054e9ca5a42a39d0887121
Thakur Saad <thakursaad613@gmail.com>
2022-09-17 23:47:26 +0600

    operator

--- app.js
+++ app.js
@@ -152,7 +152,7 @@ app.post("/api/v1/product", async (req, res, next) => {
 app.get("/api/v1/product", async (req, res, next) => {
   try {
     const products = await Product.find({
-      $or: [{ _id: "6325a76dd2e5e1aaa585f4ba" }, { name: "Mango" }],
+      name: { $in: ["Chal", "Daal"] },
     });
 
     res.status(200).json({
