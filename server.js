fabf569a2c71436b27cc78c391ffc6ed150d16a2
Thakur Saad <thakursaad613@gmail.com>
2022-09-17 23:12:55 +0600

    readme

--- server.js
+++ server.js
@@ -13,5 +13,5 @@ mongoose.connect(process.env.DATABASE_LOCAL).then(() => {
 const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
-  console.log(`App is running on port ${port}`.rainbow);
+  console.log(`App is running on port ${port}`.bgYellow.bold);
 });
