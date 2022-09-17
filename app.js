93313c82bb8c8099f398ef1b3f2335199c11affd
Thakur Saad <thakursaad613@gmail.com>
2022-09-17 10:30:26 +0600

    name

--- app.js
+++ app.js
@@ -9,7 +9,14 @@ app.use(cors());
 
 // schema design
 const productSchema = mongoose.Schema({
-
+  name: {
+    type: String,
+    required: [true, "Please provide a name for this product"],
+    trim: true,
+    unique: true,
+    minLength: [3, "Name must be at least 3 characters"],
+    maxLength: [100, "Name is too large"],
+  },
 });
 
 app.get("/", (req, res) => {
