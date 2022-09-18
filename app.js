9cd486d64553434736a9d495823ad179b16e6cf8
Thakur Saad <thakursaad613@gmail.com>
2022-09-18 12:59:29 +0600

    controller separation

--- app.js
+++ app.js
@@ -11,54 +11,8 @@ app.get("/", (req, res) => {
   res.send("Route is working! YaY!");
 });
 
-app.post("/api/v1/product", async (req, res, next) => {
-  try {
-    // save
-    // const product = new Product(req.body);
+app.post("/api/v1/product", );
 
-    // if (product.quantity == 0) {
-    //   product.status = "out-of-stock";
-    // }
-
-    // const result = await product.save();
-
-    // create
-    const result = await Product.create(req.body);
-
-    result.logger();
-
-    res.status(200).json({
-      status: "Success",
-      message: "Data inserted Successfully",
-      data: result,
-    });
-  } catch (error) {
-    console.log(error);
-
-    res.status(400).json({
-      status: "Fail",
-      message: "Data not inserted",
-      error: error.message,
-    });
-  }
-});
-
-app.get("/api/v1/product", async (req, res, next) => {
-  try {
-    const product = await Product.findById("6325a0c81e7a0ddea86416d2");
-
-    res.status(200).json({
-      status: "Success",
-      message: "Data found",
-      data: product,
-    });
-  } catch (error) {
-    res.status(400).json({
-      status: "Fail",
-      message: "Can not get the data",
-      error: error.message,
-    });
-  }
-});
+app.get("/api/v1/product", );
 
 module.exports = app;
