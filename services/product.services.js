5d5da41f4992d4bff7ef7a5516b73e559cf789dc
Thakur Saad <thakursaad613@gmail.com>
2022-09-18 21:58:13 +0600

    update

--- services/product.services.js
+++ services/product.services.js
@@ -9,3 +9,16 @@ exports.createProductService = async (data) => {
   const product = await Product.create(data);
   return product;
 };
+
+exports.updateProductService = async (productId, data) => {
+  const result = await Product.updateOne(
+    { _id: productId },
+    { $inc: data },
+    { runValidators: true }
+  );
+
+  // const product = await Product.findById(productId);
+  // const result = await product.set(data).save();
+
+  return result;
+};
