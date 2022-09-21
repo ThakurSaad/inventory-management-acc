cf0e0be997e735abd1472ac599ed5ca1448839e2
Thakur Saad <thakursaad613@gmail.com>
2022-09-21 14:20:01 +0600

    some edit

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -35,7 +35,7 @@ exports.getProducts = async (req, res, next) => {
     }
 
     if (req.query.page) {
-      const { page = 0, limit = 10 } = req.query;
+      const { page = 1, limit = 10 } = req.query;
 
       const skip = (page - 1) * parseInt(limit);
 
