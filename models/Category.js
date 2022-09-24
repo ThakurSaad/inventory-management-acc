723a77ed09ddff2c22027f32f4b9927ec8c3fc29
Thakur Saad <thakursaad613@gmail.com>
2022-09-24 13:56:51 +0600

    Category model

---
+++ models/Category.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+const { ObjectId } = mongoose.Schema.Types;
+
+const categorySchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: [true, "Please provide a category name"],
+      lowercase: true,
+      unique: true,
+    },
+    description: String,
+    imageUrl: {
+      type: String,
+      validator: [validator.isURL, "Please provide a valid URL"],
+    },
+  },
+  {
+    timestamp: true,
+  }
+);
+
+const Category = mongoose.model("Category", categorySchema);
+
+exports = Category;
