0fd0c6e8bbe18afb8fb4411ede6d745cb13e4aaa
Thakur Saad <thakursaad613@gmail.com>
2022-10-12 19:49:18 +0600

    getting ready with multer-2

--- routes/product.route.js
+++ routes/product.route.js
@@ -1,19 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const productController = require("../controllers/product.controller");
-const multer = require("multer");
-
-const uploader = multer({ dest: "images/" });
-
-router.post(
-  "/file-upload",
-  uploader.single("image"),
-  productController.fileUpload
-);
-
-// <input type="file" name="image" />;
-// const formData = new FormData();
-// formData.append("image", formData);
 
 router.route("/bulk-update").patch(productController.bulkUpdateProduct);
 router.route("/bulk-delete").delete(productController.bulkDeleteProduct);
