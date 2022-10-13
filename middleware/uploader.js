4ff9c7a30c6a21f61566eb0cbb3704e480230b6e
Thakur Saad <thakursaad613@gmail.com>
2022-10-13 10:35:22 +0600

    single file upload

---
+++ middleware/uploader.js
@@ -0,0 +1,40 @@
+const multer = require("multer");
+const path = require("path");
+
+const storage = multer.diskStorage({
+  destination: "images/",
+
+  filename: (req, file, cb) => {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, uniqueSuffix + "-" + file.originalname);
+  },
+});
+
+const uploader = multer({
+  storage,
+
+  fileFilter: (req, file, cb) => {
+    const supportedImage = /png|jpg/;
+    const extension = path.extname(file.originalname);
+
+    if (supportedImage.test(extension)) {
+      cb(null, true);
+    } else {
+      cb(new Error("Must be png/jpg"));
+    }
+  },
+
+  limits: {
+    fileSize: 5000000,
+  },
+});
+
+module.exports = uploader;
+
+/* 
+// frontend code
+
+<input type="file" name="image" />;
+const formData = new FormData();
+formData.append("image", formData); 
+*/
--- routes/product.route.js
+++ routes/product.route.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const productController = require("../controllers/product.controller");
+const uploader = require("../middleware/uploader");
+
+router.post(
+  "/file-upload",
+  uploader.single("image"),
+  productController.fileUpload
+);
 
 router.route("/bulk-update").patch(productController.bulkUpdateProduct);
 router.route("/bulk-delete").delete(productController.bulkDeleteProduct);
