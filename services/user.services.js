3ffd2d712d85cfacfeec5dc935d8648a2a87e658
Thakur Saad <thakursaad613@gmail.com>
2022-10-20 11:45:03 +0600

    findUserByEmailService

--- services/user.services.js
+++ services/user.services.js
@@ -4,3 +4,7 @@ exports.signUpService = async (userInfo) => {
   const user = await User.create(userInfo);
   return user;
 };
+
+exports.findUserByEmailService = async (email) => {
+  return await User.findOne({ email });
+};
